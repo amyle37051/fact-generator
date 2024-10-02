ee31ea29b7346978ff6c38b1ac0d34a9a2d88fc9
Amy Le <amy.ips.bt@gmail.com>
2024-10-02 19:58:48 +1300

    changed to facts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
